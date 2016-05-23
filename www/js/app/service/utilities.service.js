353f8a22fa1ae480cf96249bd6580783f110fc3c
Lars Meijdam <lmeijdam90@gmail.com>
2016-05-23 09:02:37 +0200

    utilities; created a service which contain utilities. For now it only has a method to create a guid.

---
+++ www/js/app/service/utilities.service.js
@@ -0,0 +1,24 @@
+(function() {
+'use strict';
+
+    angular
+        .module('lmTodo')
+        .factory('UtilitiesService', UtilitiesService);
+
+    UtilitiesService.$inject = [];
+    function UtilitiesService() {
+        var service = {
+            createGuid:createGuid
+        };
+        
+        return service;
+
+        ////////////////
+        function createGuid(){
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+            }
+            return s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4();
+        }       
+    }
+})();
