8cf7b9776dc81479e39efdee8b4281db59fab13f
Lars Meijdam <lmeijdam90@gmail.com>
2016-05-25 08:30:09 +0200

    services; updated TodoService to use only the public functions. mainly to improve testing

--- www/js/app/service/todo.service.js
+++ www/js/app/service/todo.service.js
@@ -1,5 +1,5 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
     angular
         .module('lmTodo')
@@ -8,78 +8,59 @@
     TodoService.$inject = ['$q', 'UtilitiesService', 'StorageService'];
     function TodoService($q, UtilitiesService, StorageService) {
         var service = {
-            getAll:getAll,
-            getSingleById:getSingleById,
-            save:save,
-            remove:remove
+            getAll: getAll,
+            getSingleById: getSingleById,
+            save: save,
+            remove: remove
         };
-        
+
         var todoList = [];
-        
+
         return service;
 
         // retrieving
-        function getAll() { 
-            return $q.when(getAllTodos());
-        }
-        
-        function getAllTodos(){
-            var storedList = StorageService.get("list");            
-            if(storedList) {
+        function getAll() {
+            var storedList = StorageService.get("list");
+            if (storedList) {
                 todoList = storedList;
             }
-            return todoList;
+            return $q.when(todoList);
         }
-        
-        function getSingleById(id){
-            for(var i = 0; i < todoList.length; i++){
-                if(todoList[i].id === id) return todoList[i];
+
+        function getSingleById(id) {
+            for (var i = 0; i < todoList.length; i++) {
+                if (todoList[i].id === id) return todoList[i];
             }
         }
-        
-        function getIndexById(id){
-            for(var i = 0; i < todoList.length; i++){
-                if(todoList[i].id === id) return i;
+
+        function getIndexById(id) {
+            for (var i = 0; i < todoList.length; i++) {
+                if (todoList[i].id === id) return i;
             }
         }
-        
+
         // saving
-        function save(todo){            
-            if(todo.id){
-                return $q.when(updateTodo(todo));
-            }            
-            return $q.when(saveTodo(todo));
-        }
-        
-        function saveTodo(todo){
-            var uid = UtilitiesService.createGuid();
-            todo.id = uid;
-            
-            todoList.push(todo);
+        function save(todo) {
+            if (!todo.id) {
+                var uid = UtilitiesService.createGuid();
+                todo.id = uid;
+                todoList.push(todo);
+            }
             
             StorageService.set("list", todoList);
+            return $q.when(todoList);
         }
-        
-        function updateTodo(todo){
-            var localTodo = getSingleById(todo.id);
-            localTodo.title = todo.title;            
-            
-            StorageService.set("list", todoList);
-        }     
         
         // deletion
         function remove(todo) {
-            return $q.when(removeTodo(todo));
-        }
-        
-        function removeTodo(todo) {
             var index = getIndexById(todo.id);
-            if(index > -1){
+            if (index > -1) {
                 todoList.splice(index, 1);
-            }            
-            
+            }
+
             StorageService.set("list", todoList);
-        }   
+            return $q.when(todoList);
+        }
     }
-    
+
 })();
